6e34f83f19516050def90fbea21a9344ee4cb0b3
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-03-01 14:33:21 -0500

    Added more variables

--- js/main.js
+++ js/main.js
@@ -8,13 +8,21 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? Enter the letter below:\na) Snacking\nb) Escaping\nc) Winning');
 
-var storyA () {
+var storyA = function () {
 
-   document.write
+   document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verbed + 'for hours.');
+
+}
+
+var storyB = funtion () {
+
+   document.write('Ameilia' + verbed + 'through the' + adjective + ' ' + colour + 'nebula escaping the space' + creature + '.');
+
 }
 
 
+var storyC = function () {
 
-Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+  document.write('Jackson chose his' + adjective + ',' + colour + ' ' + creature + 'card and' + verbed + 'it to the table knowing he won.');
 
-Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+}
