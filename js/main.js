7acb23a692d2e79904a902332da14ba9ef4b533b
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-03-01 14:10:50 -0500

    Added variables

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? Enter the letter below:\na) Snacking\nb) Escaping\nc) Winning');
+
+var storyA () {
+
+   document.write
+}
+
+
+
+Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
